d9e0a62c2d612389fbfd476d07345afa936f2e7e
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-02-26 17:48:14 -0300

    test vercel less endpoints

--- esl_bags/urls.py
+++ esl_bags/urls.py
@@ -3,11 +3,11 @@ from rest_framework.authtoken import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('user/', views.UserRetriveCreate.as_view()),
+    # path('user/', views.UserRetriveCreate.as_view()),
     path('user/login/', views.AuthLoginUser.as_view()),
-    path('user/change-password/', views.PasswordUpdate.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
-    path('user/forget-password/', views.ForgetPassword.as_view()),
-    path('user/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    # path('user/change-password/', views.PasswordUpdate.as_view()),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('user/forget-password/', views.ForgetPassword.as_view()),
+    # path('user/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 
 ]
