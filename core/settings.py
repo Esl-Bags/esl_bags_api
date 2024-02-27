16934ddfe31012edc1ed6be17ca5c7e783051945
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-02-27 10:33:36 -0300

    config statics

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -142,4 +141,4 @@ EMAIL_HOST = 'localhost'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')  
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
