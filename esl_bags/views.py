7a302f7890be708ccce85b0bd85e639ed81cbb31
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-02-27 08:57:06 -0300

    enable all endpoints

--- esl_bags/views.py
+++ esl_bags/views.py
@@ -19,28 +19,28 @@ class IsPostMethodOrAuthenticated(BasePermission):
         return False
 
 
-# class UserRetriveCreate(APIView):
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes = [IsPostMethodOrAuthenticated]
+class UserRetriveCreate(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsPostMethodOrAuthenticated]
 
-#     def get(self, request, format=None):
-#         user = UserSerializer(request.user)
-#         return Response(user.data)
+    def get(self, request, format=None):
+        user = UserSerializer(request.user)
+        return Response(user.data)
 
-#     def post(self, request, format=None):
-#         user = UserCreateSerializer(data=request.data)
-#         if user.is_valid():
-#             user.save()
-#             return Response(user.data, status=status.HTTP_201_CREATED)
-#         return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format=None):
+        user = UserCreateSerializer(data=request.data)
+        if user.is_valid():
+            user.save()
+            return Response(user.data, status=status.HTTP_201_CREATED)
+        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def patch(self, request, format=None):
-#         user = request.user
-#         serializer = UserSerializer(user, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, format=None):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AuthLoginUser(ObtainAuthToken):
@@ -57,32 +57,32 @@ class AuthLoginUser(ObtainAuthToken):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class PasswordUpdate(APIView):
-#     permission_classes = [IsAuthenticated]
-#     def patch(self, request, format=None):
-#         user = request.user
-#         data = request.data
-#         if not user.check_password(data['old_password']):
-#             return Response({'old_password': 'Senha incorreta.'}, status=status.HTTP_400_BAD_REQUEST)
-#         if user.check_password(data['new_password']):
-#             return Response({'new_password': 'A nova senha não pode ser igual a antiga.'}, status=status.HTTP_400_BAD_REQUEST)
+class PasswordUpdate(APIView):
+    permission_classes = [IsAuthenticated]
+    def patch(self, request, format=None):
+        user = request.user
+        data = request.data
+        if not user.check_password(data['old_password']):
+            return Response({'old_password': 'Senha incorreta.'}, status=status.HTTP_400_BAD_REQUEST)
+        if user.check_password(data['new_password']):
+            return Response({'new_password': 'A nova senha não pode ser igual a antiga.'}, status=status.HTTP_400_BAD_REQUEST)
 
-#         user.set_password(data['new_password'])
-#         user.save()
-#         return Response({'status': 'Senha trocada com sucesso.'})
-
-
-# class ForgetPassword(APIView):
-#     def post(self, request):
-#         return Response({'message': 'Mermão, lembra dessa senha ai q eu tô com preguiça de implementar isso.'})
+        user.set_password(data['new_password'])
+        user.save()
+        return Response({'status': 'Senha trocada com sucesso.'})
 
 
-# class ResetPassword(APIView):
-#     authentication_classes = [TokenAuthentication]
-#     def post(self, request):
-#         user = request.user
-#         data = request.data
-#         if not data['password']:
-#             Response({'password': 'Esse campo é obrigatório.'}, status=status.HTTP_400_BAD_REQUEST)
-#         if len(data['password']) < 4:
-#             pass
+class ForgetPassword(APIView):
+    def post(self, request):
+        return Response({'message': 'Mermão, lembra dessa senha ai q eu tô com preguiça de implementar isso.'})
+
+
+class ResetPassword(APIView):
+    authentication_classes = [TokenAuthentication]
+    def post(self, request):
+        user = request.user
+        data = request.data
+        if not data['password']:
+            Response({'password': 'Esse campo é obrigatório.'}, status=status.HTTP_400_BAD_REQUEST)
+        if len(data['password']) < 4:
+            pass
