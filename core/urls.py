69a04f81876e14325d23f97083a1472769813fdd
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-02-28 08:35:32 -0300

    fix bug basedir

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include("esl_bags.urls")),
     path('admin/', admin.site.urls),
-] 
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
