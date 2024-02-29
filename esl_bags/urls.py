cea3c4cab448571c561dc960700f60d6d3a04c99
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-02-29 15:18:11 -0300

    crud marca

--- esl_bags/urls.py
+++ esl_bags/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve 
 
 
 urlpatterns = [
     path('user/', include("accounts.urls")),
+    path('product/', include("products.urls")),
     path('admin/', admin.site.urls),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
