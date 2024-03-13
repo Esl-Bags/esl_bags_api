e90d99d11a1de3589870534d25a3a47622cbe0fe
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-03-13 16:19:26 -0300

    add carroussel ao painel admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from products.models import Brand, Product
+from products.models import Brand, Product, Carousel
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Product)
+admin.site.register(Carousel)
