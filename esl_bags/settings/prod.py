0ae6252b88ae1ce904a75cad65b63f63c15e60d2
FilipeNeiva <filipevilanovaneiva@gmail.com>
2024-03-14 09:16:27 -0300

    conectando db postgres

--- esl_bags/settings/prod.py
+++ esl_bags/settings/prod.py
@@ -5,6 +5,19 @@ ALLOWED_HOSTS = ['esl-bags.caprover-dev.claudinosa.com.br']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT'),
+    }
+}
+
 #Email backend configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -12,11 +25,3 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('MAIL_USER')
 EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD')
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
